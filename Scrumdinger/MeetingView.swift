3f125abc75c6733c3da51b95c368b08083d7bdbb
Yakushin Oleg <yakushinoa@gmail.com>
2023-05-17 16:46:30 +0300

    Commit

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -11,16 +11,17 @@ import AVFoundation
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
-   
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    
     var body: some View {
-        ZStack{
+        ZStack {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
-                                                  secondsRemaining: scrumTimer.secondsRemaining,
-                                                  theme: scrum.theme)
-             
+                                  secondsRemaining: scrumTimer.secondsRemaining,
+                                  theme: scrum.theme)
                 Circle()
                     .strokeBorder(lineWidth: 24)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
@@ -29,7 +30,11 @@ struct MeetingView: View {
         .padding()
         .foregroundColor(scrum.theme.accentColor)
         .onAppear {
-                    scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
             scrumTimer.startScrum()
                 }
         .onDisappear {
