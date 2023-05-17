e94e0412007dad2f13255dbe16c18949a81d0c60
Yakushin Oleg <yakushinoa@gmail.com>
2023-05-17 17:46:08 +0300

    Now all data can be saved in app data

---
+++ Scrumdinger/Models/ScrumdingerApp.swift
@@ -0,0 +1,34 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Oleg Yakushin on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @StateObject private var store = ScrumStore()
+    var body: some Scene {
+        WindowGroup {
+            ScrumsView(scrums: $store.scrums) {
+                            Task {
+                                do {
+                                try await store.save(scrums: store.scrums)
+                            } catch {
+                                fatalError(error.localizedDescription)
+                            }
+                            }
+                        }
+                .task {
+                    do {
+                    try await store.load()
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+                                }
+
+        }
+    }
+}
